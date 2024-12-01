9b4020d41b3e6d5284aa5f02083f0951954372b2
Vincent <v@jong>
2024-12-01 10:57:18 +0100

    Week 1b

--- week1.py
+++ week1.py
@@ -4,6 +4,12 @@ import re
 def do_challenge():
     file = open('1/input.txt', 'r')
     lines = file.readlines()
+    total = do_challenge_b(lines)
+        # print(f'Asbolute diff between {l_value} and {r_value} = {abs_diff}')
+    print(f'Total: {total}')
+
+
+def do_challenge_a(lines):
     total = 0
     sizes_left = []
     sizes_right = []
@@ -18,5 +24,20 @@ def do_challenge():
         r_value = sizes_right[index]
         abs_diff = abs(int(l_value) - int(r_value))
         total = total + abs_diff
-        # print(f'Asbolute diff between {l_value} and {r_value} = {abs_diff}')
-    print(f'Total: {total}')
+    return total
+
+
+def do_challenge_b(lines):
+    total = 0
+    sizes_left = []
+    sizes_right = []
+    for line in lines:
+        line = line.replace('\n', '')
+        sizes_split = line.split('   ')
+        sizes_left.append(int(sizes_split[0]))
+        sizes_right.append(int(sizes_split[1]))
+    for index, l_value in enumerate(sizes_left):
+        occurrence_count = sizes_right.count(l_value)
+        similarity = l_value * occurrence_count
+        total = total + similarity
+    return total
