d692c8bde70c8a6f1587b1895722aa5b88a9408e
Vincent <v@jong>
2024-12-03 07:34:30 +0100

    week 3b temp

--- week3.py
+++ week3.py
@@ -2,7 +2,7 @@ import re
 
 
 def do_challenge():
-    file = open('3/input.txt', 'r')
+    file = open('3/test.txt', 'r')
     lines = file.readlines()
     total = do_challenge_a(lines)
     print(f'Total: {total}')
@@ -11,8 +11,10 @@ def do_challenge():
 def do_challenge_a(lines):
     total = 0
     for line in lines:
-        mults = re.findall(r'mul\(\d+,\d+\)', line)
-        for mult in mults:
+        mults1 = re.findall(r'mul\(\d+,\d+\)(?:(?!don\'t\(\)).)*?(?=don\'t\(\))', line)
+        mults2 = re.findall(r'do\(\)(?:(?!don\'t\(\)).)*?(mul\(\d+,\d+\))', line)
+        for mult in mults1 + mults2:
+            print(f'Found mult {mult}')
             digits = re.search(r'(\d+),(\d+)', mult)
             digit1 = int(digits.group(1))
             digit2 = int(digits.group(2))
