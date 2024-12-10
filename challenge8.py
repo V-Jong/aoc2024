f09221e76914a987f73a12865101af1e36270624
Vincent <v@jong>
2024-12-10 22:05:44 +0100

    challenge 8b

--- challenge8.py
+++ challenge8.py
@@ -6,7 +6,7 @@ def do_challenge():
     start_time = time.time()
     file = open('8/input.txt', 'r')
     lines = file.readlines()
-    total = do_challenge_a(lines)
+    total = do_challenge_b(lines)
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Execution time: {execution_time:.6f} seconds")
@@ -85,8 +85,134 @@ def do_challenge_a(lines):
 
 
 def do_challenge_b(lines):
-    total = 0
-    return total
+    letters = []
+    min_x = 0
+    min_y = 0
+    max_x = 0
+    max_y = len(lines) - 1
+    for line_index, line in enumerate(lines):
+        max_x = len(line) - 1
+        line_letters = []
+        for c_index, c in enumerate(line):
+            if c.isalnum():
+                line_letters.append(Letter(c, c_index, line_index))
+        letters.append(line_letters)
+
+    flattened = [item for sublist in letters for item in sublist]
+    distinct_letters = list(set([letter.character for letter in flattened]))
+
+    antinodes = set()
+    for distinct_letter in distinct_letters:
+        sub_flattened = [letter for letter in flattened if letter.character == distinct_letter]
+        sub_combs = list(combinations(sub_flattened, 2))
+        for sub_comb in sub_combs:
+            pair_1 = sub_comb[0]
+            pair_2 = sub_comb[1]
+            p1_x = pair_1.x
+            p1_y = pair_1.y
+            p2_x = pair_2.x
+            p2_y = pair_2.y
+            if p1_x == p2_x:
+                y_dist = abs(p1_y - p2_y)
+                if p1_y < p2_y:
+                    new_p1_y = p1_y - y_dist
+                    while new_p1_y >= min_y:
+                        antinodes.add((p1_x, new_p1_y))
+                        new_p1_y = new_p1_y - y_dist
+                    new_p2_y = p2_y + y_dist
+                    while new_p2_y <= max_y:
+                        antinodes.add((p1_x, new_p2_y))
+                        new_p2_y = new_p2_y + y_dist
+                else:
+                    new_p2_y = p2_y - y_dist
+                    while new_p2_y >= min_y:
+                        antinodes.add((p1_x, new_p2_y))
+                        new_p2_y = new_p2_y - y_dist
+                    new_p1_y = p1_y + y_dist
+                    while new_p1_y <= max_y:
+                        antinodes.add((p1_x, new_p1_y))
+                        new_p1_y = new_p1_y + y_dist
+            elif p1_y == p2_y:
+                x_dist = abs(p1_x - p2_x)
+                if p1_x < p2_x:
+                    new_p1_x = p1_x - x_dist
+                    while new_p1_x >= min_x:
+                        antinodes.add((new_p1_x, p1_y))
+                        new_p1_x = new_p1_x - x_dist
+                    new_p2_x = p2_x + x_dist
+                    while new_p2_x <= max_x:
+                        antinodes.add((new_p2_x, p1_y))
+                        new_p2_x = new_p2_x + x_dist
+                else:
+                    new_p2_x = p2_y - x_dist
+                    while new_p2_x >= min_x:
+                        antinodes.add((new_p2_x, p1_y))
+                        new_p2_x = new_p2_x - x_dist
+                    new_p1_x = p1_y + x_dist
+                    while new_p1_x <= max_x:
+                        antinodes.add((new_p1_x, p1_y))
+                        new_p1_x = new_p1_x + x_dist
+            elif p1_x < p2_x:
+                x_dist = abs(p1_x - p2_x)
+                y_dist = abs(p1_y - p2_y)
+                if p1_y < p2_y:
+                    new_p1_x = p1_x - x_dist
+                    new_p1_y = p1_y - y_dist
+                    while min_x <= new_p1_x <= max_x and min_y <= new_p1_y <= max_y:
+                        antinodes.add((new_p1_x, new_p1_y))
+                        new_p1_x = new_p1_x - x_dist
+                        new_p1_y = new_p1_y - y_dist
+                    new_p2_x = p2_x + x_dist
+                    new_p2_y = p2_y + y_dist
+                    while min_x <= new_p2_x <= max_x and min_y <= new_p2_y <= max_y:
+                        antinodes.add((new_p2_x, new_p2_y))
+                        new_p2_x = new_p2_x + x_dist
+                        new_p2_y = new_p2_y + y_dist
+                else:
+                    new_p1_x = p1_x - x_dist
+                    new_p1_y = p1_y + y_dist
+                    while min_x <= new_p1_x <= max_x and min_y <= new_p1_y <= max_y:
+                        antinodes.add((new_p1_x, new_p1_y))
+                        new_p1_x = new_p1_x - x_dist
+                        new_p1_y = new_p1_y + y_dist
+                    new_p2_x = p2_x + x_dist
+                    new_p2_y = p2_y - y_dist
+                    while min_x <= new_p2_x <= max_x and min_y <= new_p2_y <= max_y:
+                        antinodes.add((new_p2_x, new_p2_y))
+                        new_p2_x = new_p2_x + x_dist
+                        new_p2_y = new_p2_y - y_dist
+            else:
+                x_dist = abs(p1_x - p2_x)
+                y_dist = abs(p1_y - p2_y)
+                if p1_y < p2_y:
+                    new_p1_x = p1_x + x_dist
+                    new_p1_y = p1_y - y_dist
+                    while min_x <= new_p1_x <= max_x and min_y <= new_p1_y <= max_y:
+                        antinodes.add((new_p1_x, new_p1_y))
+                        new_p1_x = new_p1_x + x_dist
+                        new_p1_y = new_p1_y - y_dist
+                    new_p2_x = p2_x - x_dist
+                    new_p2_y = p2_y + y_dist
+                    while min_x <= new_p2_x <= max_x and min_y <= new_p2_y <= max_y:
+                        antinodes.add((new_p2_x, new_p2_y))
+                        new_p2_x = new_p2_x - x_dist
+                        new_p2_y = new_p2_y + y_dist
+                else:
+                    new_p1_x = p1_x + x_dist
+                    new_p1_y = p1_y + y_dist
+                    while min_x <= new_p1_x <= max_x and min_y <= new_p1_y <= max_y:
+                        antinodes.add((new_p1_x, new_p1_y))
+                        new_p1_x = new_p1_x + x_dist
+                        new_p1_y = new_p1_y + y_dist
+                    new_p2_x = p2_x - x_dist
+                    new_p2_y = p2_y - y_dist
+                    while min_x <= new_p2_x <= max_x and min_y <= new_p2_y <= max_y:
+                        antinodes.add((new_p2_x, new_p2_y))
+                        new_p2_x = new_p2_x - x_dist
+                        new_p2_y = new_p2_y - y_dist
+
+    antinodes.update(set([(letter.x, letter.y) for letter in flattened]))
+    return len(antinodes)
 
 
 class Letter:
